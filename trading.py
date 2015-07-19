cef90f9770ec6c245ac25aa154aef88f8fcc9ab5
tsu-nera <fox10225fox@gmail.com>
2015-07-19 14:20:17 +0900

    フォワードテスト用ソースの整理

--- trading.py
+++ trading.py
@@ -6,14 +6,13 @@ from execution import OANDAExecutionHandler
 from settings import ACCESS_TOKEN, ACCOUNT_ID
 from streaming import StreamingForexPrices
 
-from main import Main
 from sma_strategy import SMAStrategy
 from portfolio import PortfolioRemote
 
 heartbeat = 0.5
 
 
-def on_tick(self, event_queue, strategies, progress=None):
+def on_tick(event_queue, strategies, portfolio):
     while True:
         while not event_queue.empty():
             event = event_queue.get(False)
@@ -22,6 +21,7 @@ def on_tick(self, event_queue, strategies, progress=None):
             for strategy in strategies:
                 if(strategy.check(event)):
                     break
+            portfolio.print_status(event)
 
         time.sleep(heartbeat)
 
@@ -33,18 +33,16 @@ if __name__ == "__main__":
 
     status = dict()  # tick をまたいで記憶しておきたい情報
 
-    # 戦略
-    strategies = set([SMAStrategy(events, status)])
-
     portfolio = PortfolioRemote(status)  # お金管理
 
     execution = OANDAExecutionHandler(status)  # 売買注文
 
-    main = Main()
+    # 戦略
+    strategy = SMAStrategy(events, status, execution, portfolio)
+    strategies = set([strategy])
 
-    trade_thread = threading.Thread(target=main.on_tick,
-                                    args=[events, strategies,
-                                          execution, portfolio])
+    trade_thread = threading.Thread(target=on_tick,
+                                    args=[events, strategies, portfolio])
 
     price_thread = threading.Thread(target=prices.begin, args=[
         ACCOUNT_ID, "EUR_USD", events
